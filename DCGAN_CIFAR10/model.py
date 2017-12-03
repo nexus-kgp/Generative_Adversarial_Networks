767b9ed41110ef225130d4a895499ac932dfbf1d
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2017-12-03 14:23:54 +0530

    generator

--- DCGAN_CIFAR10/model.py
+++ DCGAN_CIFAR10/model.py
@@ -76,3 +76,81 @@ def conv2d(name, bottom, shape, strides, top_shape=None, with_bn=True, is_train=
         top = tf.nn.bias_add(conv, biases)
 
     return top
+
+
+
+def linear(name, bottom, shape, with_bn=True, is_train=None):
+    """
+        Creates a fully connected + BN block.
+    """
+
+    with tf.variable_scope(name) as scope:
+
+        # apply batch normalization, if necessary
+        bn = _bn(bottom, is_train) if with_bn else bottom
+
+        # inner product
+        weights = tf.get_variable("weights", shape=shape,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.02))
+        linear = tf.matmul(bn, weights)
+
+        # add biases
+        bias_shape = [shape[-1]]
+        biases = tf.get_variable("biases", shape=bias_shape,
+                                 initializer=tf.constant_initializer())
+        top = tf.nn.bias_add(linear, biases)
+
+    return top
+
+
+def generator(data, is_train, side_length):
+    """
+        Builds the generator model.
+
+        :param tensorflow.Tensor data:
+            A 4-D input tensor with the last dimension Gaussian sampled.
+
+        :param tensorflow.Tensor is_train:
+            Determines whether or not the generator is used only for inference.
+
+        :param int side_length:
+            Image side length.
+    """
+
+    assert side_length % 16 == 0, "image side length must be divisible by 16"
+
+    dim = side_length / 16
+    (batch_size, z_len) = data.get_shape().as_list()
+
+    # project block, 1024 outputs
+    proj = tf.nn.relu(linear("g_proj", data, [z_len, dim * dim * 1024], with_bn=False))
+    rshp = tf.reshape(proj, [batch_size, dim, dim, 1024])
+
+    # conv1 block, 512 outputs
+    dim *= 2
+    conv1 = tf.nn.relu(conv2d("g_conv1", rshp, [5, 5, 512, 1024], STRIDE_2,
+                              top_shape=[batch_size, dim, dim, 512], is_train=is_train))
+
+    # conv2 block, 256 outputs
+    dim *= 2
+    conv2 = tf.nn.relu(conv2d("g_conv2", conv1, [5, 5, 256, 512], STRIDE_2,
+                              top_shape=[batch_size, dim, dim, 256], is_train=is_train))
+
+    # conv3 block, 128 outputs
+    dim *= 2
+    conv3 = tf.nn.relu(conv2d("g_conv3", conv2, [5, 5, 128, 256], STRIDE_2,
+                              top_shape=[batch_size, dim, dim, 128], is_train=is_train))
+
+    # conv4 block, 64 outputs
+    dim *= 2
+    conv4 = tf.nn.relu(conv2d("g_conv4", conv3, [5, 5, 64, 128], STRIDE_2,
+                              top_shape=[batch_size, dim, dim, 64], is_train=is_train))
+
+    # conv5 block, 3 outputs
+    conv5 = tf.nn.tanh(conv2d("g_conv5", conv4, [3, 3, 3, 64], STRIDE_1,
+                              top_shape=[batch_size, dim, dim, 3], is_train=is_train))
+
+    top = conv5
+
+    return top
+
